ffe70b4865f4f6031c09c432d68c3430969be851
lenemson <jibanez@student.42.fr>
2015-10-28 23:28:30 +0100

    Refactoring and a bit of error handling in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,95 +12,107 @@
 
 #!/bin/sh
 
-# Color for stdout
+# Colors for output
 GRN="\033[0;32m"
 BLU="\033[0;34m"
 RED="\033[0;31m"
 YEL="\033[0;33m"
+PUR="\033[0;35m"
 NON="\033[0m"
 
-# Ruby version wanted
-RUBY_V="2.2.3"
-
 # Gets the absolute path of the dotfiles directory
+# Eg. ~/.dotfiles
 DOTFILES_DIR="$(cd "$(dirname $0)" && pwd)"
 
-# Creates oldrc directory to store saved config files
-mkdir -p $DOTFILES_DIR/oldrc
+# Creates an `oldrc` directory to store old config files/directories
+# This directory will be ignored by git
+mkdir -p $DOTFILES_DIR/oldrc/.oh-my-zsh
 
-# Prints to stdout and execute command
-exe() { echo $YEL"$@"$NON ; "$@" ; }
+# Prints a title
+title() { echo "\n"$BLU"$@:"$NON ; }
 
-# Saves .*rc file if exist
-save() {
-	if [ -f $@ ]; then
+# Prints an information
+info() { echo $PUR": $@"$NON ; }
+
+# Prints a warning
+warning() { echo $RED"warning: $@"$NON ; }
+
+# Prints && executes a command
+exe() { echo $YEL"> $@"$NON ; "$@" ; }
+
+# Tries to git clone
+trygit() { exe git clone $@ 2>/dev/null || warning "git repository already exists" ; }
+
+# Saves a config (.*rc) file if already exists
+savefile() {
+	if [ -f $HOME/$@ ]; then # If file already exists
 		mv $HOME/$@ $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")
-		echo "old $@ saved in $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")"
+		info "old $@ saved in $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")"
 	fi
 }
 
-#
-echo $GRN"Doftfiles git repository path: $DOTFILES_DIR"$NON
+# Saves a config directory if already exists
+savedir() {
+	if [ -d $HOME/$@ ]; then # If directory already exists
+		mv $HOME/$@ $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")
+		info "old $@ saved in $DOTFILES_DIR/oldrc/$@.$(date +"%Y-%m-%d_%H-%M-%S")"
+	fi
+}
+
+# (-(-.(-.-).-)-)
+echo $GRN"> Doftfiles git repository path: $DOTFILES_DIR"$NON
 
 # Git
-echo "\n$BLU Git: $NON"
-cd $HOME
+title "Git"
 # Symlinks git config files
-save .gitconfig
-exe ln -s $DOTFILES_DIR/.gitconfig .
-save .gitignore_global
-exe ln -s $DOTFILES_DIR/.gitignore_global .
+savefile .gitconfig
+exe ln -s $DOTFILES_DIR/.gitconfig $HOME
+savefile .gitignore_global
+exe ln -s $DOTFILES_DIR/.gitignore_global $HOME
+info "Git config done!"
 
 # Vim
-echo "\n$BLU Vim: $NON"
+title "Vim"
 # Symlinks .vim directory
-exe ln -s $DOTFILES_DIR/.vim .
+savedir .vim
+exe ln -s $DOTFILES_DIR/.vim $HOME
 # Install/Clone Vundle into .vim/bundle
-exe git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+trygit https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 # Symlinks vimrc
-save .vimrc
-exe ln -s .vim/vimrc .vimrc
+savefile .vimrc
+exe ln -s $HOME/.vim/vimrc $HOME/.vimrc
 # Install vim plugin via Vundle
 exe vim +PluginInstall +qall
+info "Vim config done!"
 
 # Ruby
-echo "\n$BLU Ruby: $NON"
-cd $HOME
+title "Ruby"
 # Symlinks gemrc
-save .gemrc
-exe ln -s $DOTFILES_DIR/.gemrc .
+savefile .gemrc
+exe ln -s $DOTFILES_DIR/.gemrc $HOME
 # Install rbenv
-exe git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-# Install ruby-build (rbenv install)
-exe git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-# Install ruby
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-#exe rbenv install $RUBY_V
-#exe rbenv global $RUBY_V
-
-# End
-echo $GRN"\n\nTo Do:\n"
-echo "- Install solarized for iTerm2\n\t-> https://github.com/altercation/solarized"
-echo "- Install ruby\n\t-> rbenv install 2.2.3 && rbenv global 2.2.3"
-echo "- Install brew\n\t-> ruby -e \$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"$NON
+trygit https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+# Install ruby-build (rbenv install enhancer)
+trygit https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+info "Ruby config done!"
 
 # Zsh
-echo "\n$BLU Zsh: $NON"
-# Install oh my zsh if needed
-if [ -d $HOME/.oh-my-zsh ]; then
-	echo "Oh My Zsh already installed"
-else
-	echo "Installing Oh My Zsh"
-	exe git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-	#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+title "Zsh"
+# Install/Clone oh my zsh
+trygit https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 # Symlinks zshrc
-save .zshrc
-exe ln -s $DOTFILES_DIR/.zshrc .
+savefile .zshrc
+exe ln -s $DOTFILES_DIR/.zshrc $HOME
 # Symlinks Oh My Zsh custom directory, save the current
-cd .oh-my-zsh/
-mv custom $DOTFILES_DIR/oldrc/custom.$(date +"%Y-%m-%d_%H-%M-%S")
-exe ln -s $DOTFILES_DIR/.oh-my-zsh/custom .
+savedir .oh-my-zsh/custom
+exe ln -s $DOTFILES_DIR/.oh-my-zsh/custom $HOME/.oh-my-zsh
+info "Zsh config done!"
+
+# To Do
+title "To Do"
+echo "- Install solarized for iTerm2\n\t-> https://github.com/altercation/solarized"
+echo "- Install ruby\n\t-> rbenv install 2.2.3 && rbenv global 2.2.3"
+echo "- Install brew\n\t-> ruby -e \$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 cd $HOME
+# Run a new zsh processus to load the new .zshrc
 exec zsh
